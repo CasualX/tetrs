8e5b6deb90af33f98df6654c16f783e9a00c538b
CasualX <CasualX@GitHub.com>
2017-04-06 19:58:37 +0200

    Wrote Piece documentation.

--- tetrs/src/piece.rs
+++ tetrs/src/piece.rs
@@ -1,6 +1,9 @@
 
 use ::std::mem;
 
+/// Piece sprite.
+///
+/// The sprite pixels are 4x4 with only the low nibble used.
 pub struct Sprite {
 	pub pix: [u8; 4],
 }
@@ -8,7 +11,58 @@ pub struct Sprite {
 /// All the valid tetrominoes.
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 #[repr(u8)]
-pub enum Piece { O, I, S, Z, L, J, T }
+pub enum Piece {
+	/// The `O` tetromino.
+	///
+	/// ```text
+	/// □□
+	/// □□
+	/// ```
+	O,
+	/// The `I` tetromino.
+	///
+	/// ```text
+	/// □□□□
+	/// ```
+	I,
+	/// The `S` tetromino.
+	///
+	/// ```text
+	///  □□
+	/// □□
+	/// ```
+	S,
+	/// The `Z` tetromino.
+	///
+	/// ```text
+	/// □□
+	///  □□
+	/// ```
+	Z,
+	/// The `L` tetromino.
+	///
+	/// ```text
+	/// □
+	/// □
+	/// □□
+	/// ```
+	L,
+	/// The `J` tetromino.
+	///
+	/// ```text
+	///  □
+	///  □
+	/// □□
+	/// ```
+	J,
+	/// The `T` tetromino.
+	///
+	/// ```text
+	/// □□□
+	///  □
+	/// ```
+	T,
+}
 
 impl ::rand::Rand for Piece {
 	fn rand<R: ::rand::Rng>(rng: &mut R) -> Piece {
